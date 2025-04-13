5b84a986cc9cb4da2a2b76b6b94eda9d987e27c8
DavidCG5 <98667227+DavidCG5@users.noreply.github.com>
2025-04-13 14:37:15 -0500

    feat: corregir nombre del componente y título en la página Sobre Mi; agregar configuración de cSpell

--- src/app/sobremi/page.tsx
+++ src/app/sobremi/page.tsx
@@ -1,7 +1,7 @@
-export default function Sobremi() {
+export default function SobreMi() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold">Sobremi</h1>
+      <h1 className="text-6xl font-bold">Sobre Mi</h1>
       <p className="text-2xl">
         Esta es una página de prueba en Next.js.
       </p>
